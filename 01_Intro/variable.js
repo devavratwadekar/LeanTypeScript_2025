1f48a690feaf6ae810a5e3be29fe249cc58b9412
devavratwadekar <devavrat.wadekar@gmail.com>
2025-01-30 23:35:51 +0530

    variable in typescript - commit 2

---
+++ 01_Intro/variable.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greeting = "Hello Devavrat";
+var myNumber = 10;
+console.log(greeting.toUpperCase());
+console.log(myNumber);
---
+++ 01_Intro/variable.ts
@@ -0,0 +1,9 @@
+let greeting: string = "Hello Devavrat";
+
+let myNumber: number = 10;
+
+console.log(greeting.toUpperCase());
+console.log(myNumber);
+
+
+export { }
