0c80e8393f4301cf6fb01db7833a72ec336d0adc
devavratwadekar <devavrat.wadekar@gmail.com>
2025-01-30 23:08:19 +0530

    Learn TypeScript - commit 1

---
+++ 01_Intro/intro.js
@@ -0,0 +1,3 @@
+var user = { name: "Devavrat", age: 20 };
+console.log("Name : " + user.name);
+console.log("Age : " + user.age);
---
+++ 01_Intro/intro.ts
@@ -0,0 +1,4 @@
+var user = {name : "Devavrat", age : 20}
+
+console.log("Name : " + user.name);
+console.log("Age : " + user.age);
