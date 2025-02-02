16fef2124e06eb379845e10e9b126135aec629d3
devavratwadekar <devavrat.wadekar@gmail.com>
2025-02-02 20:24:25 +0530

    funcation in typescript - commit 3

--- 02_Basics/myFuncation.js
+++ 02_Basics/myFuncation.js
@@ -3,16 +3,38 @@ Object.defineProperty(exports, "__esModule", { value: true });
 function addTwoNumber(num) {
     return num + 5;
 }
-var addition = addTwoNumber(5);
-console.log("Addition = " + addition);
+console.log("Addition = " + addTwoNumber(7));
 function yourFullName(fname, mname, lname) {
     return fname + " " + mname + " " + lname;
 }
-var fullName = yourFullName("Devavrat", "Kishor", "Wadekar");
-console.log("Full Name : " + fullName);
-function signUpUser(name, email, isPaid) {
-}
+console.log("Full Name : " + yourFullName("Devavrat", "Kishor", "Wadekar"));
+function signUpUser(name, email, isPaid) { }
 var loginUser = function (name, age, isLogin) {
     if (isLogin === void 0) { isLogin = false; }
-    // return "Name : "+name +" Age : "+age+" Status : "+isLogin;
+    return "Name : " + name + " Age : " + age + " Status : " + isLogin;
 };
+// Multiple return type
+function getValue(myValue) {
+    if (myValue > 5) {
+        return true;
+    }
+    return "200 OK";
+}
+// Arrow Function
+var getHello = function (s) {
+    return "";
+};
+var heros = ["Thor", "Spider-Man", "Iron-Man"];
+heros.map(function (hero) {
+    return "hero is ".concat(hero);
+});
+// Use void
+function consoleError(errmsg) {
+    console.log(errmsg);
+}
+console.log("Console Error Message ".concat(consoleError("#404")));
+// Use never
+function handleError(errmsg) {
+    throw new Error(errmsg);
+}
+console.log("Console Error Message ".concat(handleError("#404")));
--- 02_Basics/myFuncation.ts
+++ 02_Basics/myFuncation.ts
@@ -1,22 +1,54 @@
-function addTwoNumber(num: number) {
+function addTwoNumber(num: number): number {
     return num + 5;
 }
-let addition: number = addTwoNumber(5);
-console.log("Addition = " + addition);
+console.log("Addition = " + addTwoNumber(7));
 
 
-function yourFullName(fname: string, mname: string, lname: string) {
+function yourFullName(fname: string, mname: string, lname: string): string {
     return fname + " " + mname + " " + lname;
 }
-let fullName = yourFullName("Devavrat", "Kishor", "Wadekar");
-console.log("Full Name : " + fullName);
+console.log("Full Name : " + yourFullName("Devavrat", "Kishor", "Wadekar"));
 
-function signUpUser(name: string, email: string, isPaid: boolean) {
+function signUpUser(name: string, email: string, isPaid: boolean) { }
 
+let loginUser = (name: string, age: number, isLogin: boolean = false) => {
+    return "Name : " + name + " Age : " + age + " Status : " + isLogin;
 }
 
-let loginUser= (name: string, age: number, isLogin: boolean = false) => { 
-    // return "Name : "+name +" Age : "+age+" Status : "+isLogin;
+
+// Multiple return type
+function getValue(myValue: number): boolean | string {
+    if (myValue > 5) {
+        return true
+    }
+    return "200 OK"
 }
+
+// Arrow Function
+const getHello = (s: string): string{
+    return "";
+}
+
+
+const heros = ["Thor", "Spider-Man", "Iron-Man"]
+heros.map((hero): string => {
+    return `hero is ${hero}`
+})
+
+
+// Use void
+function consoleError(errmsg: string): void {
+    console.log(errmsg);
+}
+console.log(`Console Error Message ${consoleError("#404")}`);
+
+// Use never
+function handleError(errmsg: string): never {
+    throw new Error(errmsg);
+}
+console.log(`Console Error Message ${handleError("#404")}`);
+
+
+
 
 export { }
